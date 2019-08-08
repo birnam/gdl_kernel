e43fe643192937df7fdb571123e8f9d2e535d277
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2019-08-08 16:31:04 +0200

    dome IDL->GDL name changes

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import json
 import os.path
 import sys
 
-kernel_json = {"argv":[sys.executable,"-m","idl_kernel","-f","{connection_file}"],
-               "display_name":"IDL",
-               "language":"IDL",
-               "codemirror_mode":"idl"}
+kernel_json = {"argv":[sys.executable,"-m","gdl_kernel","-f","{connection_file}"],
+               "display_name":"GDL",
+               "language":"GDL",
+               "codemirror_mode":"gdl"}
 
 class install_with_kernelspec(install):
     def run(self):
@@ -15,7 +15,7 @@ class install_with_kernelspec(install):
 
         from jupyter_client.kernelspec import KernelSpecManager
         from IPython.utils.path import ensure_dir_exists
-        destdir = os.path.join(KernelSpecManager().user_kernel_dir,'IDL')
+        destdir = os.path.join(KernelSpecManager().user_kernel_dir,'GDL')
         ensure_dir_exists(destdir)
         with open(os.path.join(destdir,'kernel.json'),'w') as f:
             json.dump(kernel_json,f,sort_keys=True)
@@ -28,13 +28,13 @@ svem_flag = '--single-version-externally-managed'
 if svem_flag in sys.argv:
     sys.argv.remove(svem_flag)
 
-setup(name='IDL_kernel',
+setup(name='GDL_kernel',
       version='0.5',
-      description='A IDL kernel for IPython',
+      description='A GDL kernel for Jupyter',
       long_description=readme,
-      author='Luke Stagner',
-      url='https://github.com/lstagner/idl_kernel',
-      py_modules=['idl_kernel'],
+      author='Luke Stagner, GDL Team',
+      url='https://github.com/gnudatalanguage/gdl_kernel',
+      py_modules=['gdl_kernel'],
       cmdclass={'install': install_with_kernelspec},
 #      install_requires=['pexpect>=3.3','IPython >= 3.0'],
       classifiers = [
